aacffd53ed378d5e089299a90d6655d1d5f1d1b6
Obamina <sleekkab@gmail.com>
2024-03-15 22:32:10 -0400

    init

---
+++ tom/create.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+<<core
+This script is intended to clone git directory from a remote repository to a local repository on developers computer
+only users with priviledges should run this script
+core
